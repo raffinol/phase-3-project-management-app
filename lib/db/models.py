30d962d772bd60be3b40db692b4736e7c0498f71
Rafael Finol <rafaelfinol88@gmail.com>
2023-08-19 22:55:45 -0400

    add variables to project table

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -7,4 +8,9 @@ class Project(Base):
     __tablename__ = 'project'
 
     id = Column(Integer, primary_key=True)
+    title = Column(String(50))
+    description = Column(String(150))
+    start_date = Column(DateTime)
+    due_date = Column(DateTime)
+    urgency = Column(String(10))
     
