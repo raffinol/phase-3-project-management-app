b170d322971438429cfb9e6af3ae440f2c14c84f
Rafael Finol <rafaelfinol88@gmail.com>
2023-08-21 22:01:26 -0400

    add repr method to project and engineer methods

--- lib/db/models.py
+++ lib/db/models.py
@@ -4,8 +4,9 @@ from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
+
 class Project(Base):
-    __tablename__ = 'project'
+    __tablename__ = "project"
 
     id = Column(Integer, primary_key=True)
     title = Column(String(50))
@@ -14,12 +15,33 @@ class Project(Base):
     due_date = Column(DateTime)
     urgency = Column(String(10))
 
+    def __repr__(self):
+        return (
+            f"\n<project "
+            + f"id={self.id}, "
+            + f"title={self.title}, "
+            + f"description={self.description}, "
+            + f"start_date={self.start_date}, "
+            + f"due_date={self.due_date}, "
+            + f"urgency={self.urgency}, "
+            + " >"
+        )
+
+
 class Engineers(Base):
-    __tablename__ = 'engineer'
+    __tablename__ = "engineer"
 
     id = Column(Integer, primary_key=True)
+    name = Column(String)
+    last_name = Column(String)
+    level = Column(String(10))
 
-
-
-
-    
+    def __repr__(self):
+        return (
+            f"\n<engineer "
+            + f"id={self.id}, "
+            + f"name={self.name}, "
+            + f"last_name={self.last_name}, "
+            + f"level={self.level}, "
+            + " >"
+        )
