7409c90b1c31ddb5576ebef99655d42a6a6bdeec
Rafael Finol <rafaelfinol88@gmail.com>
2023-08-22 09:54:37 -0400

    add project table seed data in seed.py

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -11,6 +11,9 @@ from faker import Faker
 
 fake = Faker()
 
+session.query(Engineers).delete()
+session.query(Project).delete()
+
 levels = ["junior", "mid", "senior"]
 for _ in range(5):
     first_name = fake.first_name()
@@ -18,6 +21,20 @@ for _ in range(5):
     engineer = Engineers(name=first_name, last_name=last_name, level=random(levels))
     print(engineer)
 
+urgency = ["high", "medium", "low"]
+for _ in range(10):
+    title = fake.text(max_nb_chars=50)
+    description = fake.text(max_nb_chars=150)
+    start_date = fake.past_date()
+    due_date = fake.future_date()
+    project = Project(
+        title=title,
+        description=description,
+        start_date=start_date,
+        due_date=due_date,
+        urgency=random(urgency),
+    )
+    print(project)
 
 import ipdb
 
