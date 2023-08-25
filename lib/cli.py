4eb8021ee3885b2d3cf14632cfe1fb878ce12bcd
Rafael Finol <rafaelfinol88@gmail.com>
2023-08-25 00:21:58 -0400

    create main_menu function in cli.py

---
+++ lib/cli.py
@@ -0,0 +1,9 @@
+def main_menu():
+    print("Welcome to Project Tracker!\n")
+    print("1. See list of projects")
+    print("2. See list of engineers")
+    print("3. Exit")
+    user_input = input("Please make a selection (1-3)")
+
+
+main_menu()
