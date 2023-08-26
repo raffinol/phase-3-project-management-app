178ff4deeebb9a8ff136b38f5cf39881ea1959ec
Rafael Finol <rafaelfinol88@gmail.com>
2023-08-25 22:00:05 -0400

    see a list of all engineers

--- lib/cli.py
+++ lib/cli.py
@@ -1,5 +1,5 @@
 from prettycli import red, green
-from db.models import Project
+from db.models import Project, Engineers
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import pandas as pd
@@ -12,7 +12,8 @@ session = Session()
 def main_menu():
     print("Welcome to Project Tracker!\n")
     print("1. See list of projects")
-    print("2. Exit")
+    print("2. See list of engineers")
+    print("3. Exit")
     user_input = input("Please make a selection (1-3)\n")
 
     handle_user_input(user_input)
@@ -22,8 +23,11 @@ def handle_user_input(input):
     is_number = input.isdigit()
     if is_number:
         selection = int(input)
-        if 1 <= selection <= 2:
+        if 1 <= selection <= 3:
             handle_selection(selection)
+        else:
+            print(red("Incorrect selection\n"))
+            main_menu()
     else:
         print(red("Incorrect selection\n"))
         main_menu()
@@ -32,6 +36,8 @@ def handle_user_input(input):
 def handle_selection(selection):
     if selection == 1:
         projects_list()
+    elif selection == 2:
+        engineers_list()
     else:
         exit()
 
@@ -47,8 +53,19 @@ def projects_list():
     main_menu()
 
 
+def engineers_list():
+    engineers = []
+
+    for engineer in session.query(Engineers):
+        engineers.append({"id": engineer.id, "name": engineer.name})
+        df = pd.DataFrame(engineers)
+    print(df.to_string(index=False))
+    print("\n")
+    main_menu()
+
+
 def exit():
-    print(green("good bye!"))
+    print(green("goodbye!"))
 
 
 main_menu()
